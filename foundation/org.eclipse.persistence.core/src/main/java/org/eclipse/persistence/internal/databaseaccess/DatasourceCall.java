1fc97ef5659243cb77d490b96bb34999293997d3
Dmitry Taimanov <taimanov@haulmont.com>
2023-04-05 12:54:47 +0600

    PL-6459 Native SQL query error if ? parameter alias is followed by \r symbol Konstantin Krivopustov* 14.01.2016, 16:43 59df7a7a

--- foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/internal/databaseaccess/DatasourceCall.java
+++ foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/internal/databaseaccess/DatasourceCall.java
@@ -928,7 +928,7 @@ public abstract class DatasourceCall implements Call {
      * Return the characters that represent non-arguments names.
      */
     protected String whitespace() {
-        return ",); \n\t:";
+        return ",); \r\n\t:";// jmix: added \r
     }
 
     /**
