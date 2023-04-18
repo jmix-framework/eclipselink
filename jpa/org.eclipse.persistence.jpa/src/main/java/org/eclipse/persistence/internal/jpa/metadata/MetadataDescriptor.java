a0579b47a2920e560b6484a53f14ea9caae9d3e8
Dmitry Taimanov <taimanov@haulmont.com>
2023-04-19 01:09:10 +0600

    PL-6381 Internal eclipselink enhancer error Andrey Subbotin 07.06.2016, 18:49 c5e91fa5

--- jpa/org.eclipse.persistence.jpa/src/main/java/org/eclipse/persistence/internal/jpa/metadata/MetadataDescriptor.java
+++ jpa/org.eclipse.persistence.jpa/src/main/java/org/eclipse/persistence/internal/jpa/metadata/MetadataDescriptor.java
@@ -127,6 +127,7 @@ import org.eclipse.persistence.descriptors.DescriptorEventListener;
 import org.eclipse.persistence.descriptors.RelationalDescriptor;
 import org.eclipse.persistence.descriptors.ReturningPolicy;
 import org.eclipse.persistence.descriptors.SingleTableMultitenantPolicy;
+import org.eclipse.persistence.exceptions.DescriptorException;
 import org.eclipse.persistence.exceptions.ValidationException;
 import org.eclipse.persistence.internal.descriptors.OptimisticLockingPolicy;
 import org.eclipse.persistence.internal.descriptors.VirtualAttributeMethodInfo;
@@ -1043,6 +1044,12 @@ public class MetadataDescriptor {
      * It assumes there is one.
      */
     public DatabaseField getPrimaryKeyField() {
+        // jmix begin
+        List<DatabaseField> primaryKeys = getPrimaryKeyFields();
+        if (primaryKeys.isEmpty()) {
+            throw DescriptorException.primaryKeyFieldsNotSepcified(m_descriptor);
+        }
+        // jmix end
         return getPrimaryKeyFields().iterator().next();
     }
 
