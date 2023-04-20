8714c989cb4ffb5e262af7972014e2e2fcf063c4
Dmitry Taimanov <taimanov@haulmont.com>
2023-04-20 15:11:20 +0600

    PL-8552 Multiple links to one entity might be set incorrectly in certain conditions subbotin 04.12.2019, 19:22 3c58503b (ac274899 partially)

--- foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/eis/EISMappedRecord.java
+++ foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/eis/EISMappedRecord.java
@@ -120,6 +120,12 @@ public class EISMappedRecord extends AbstractRecord {
         return getRecord().get(key);
     }
 
+    // jmix begin
+    public Object get(DatabaseField key, boolean lookupField) {
+        return get(key);
+    }
+    // jmix end
+
     /**
      * Forward the request to the record.
      * Wrapped nested records and collections to SDKFieldValues.
